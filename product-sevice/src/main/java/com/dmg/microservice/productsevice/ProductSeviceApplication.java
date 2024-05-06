1f0346ca1cf41ac99f2f5211251638505fd36f8b
DMGamage <gamagemadhushan@gmail.com>
2024-05-06 23:40:35 +0530

    Initial commit

---
+++ product-sevice/src/main/java/com/dmg/microservice/productsevice/ProductSeviceApplication.java
@@ -0,0 +1,13 @@
+package com.dmg.microservice.productsevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSeviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductSeviceApplication.class, args);
+    }
+
+}
